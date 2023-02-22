ff25423583cfd518e986eca28a1889bb1cc7ebad
ker4ik13 <kekustop228@mail.ru>
2023-02-22 23:45:56 +0300

    Add page 3 and page 4. Add settings and profile in NavBar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 const cardItem = document.querySelector('.card__item'),
     cardItems = document.querySelectorAll('card__item'),
-    cardItemActive = document.querySelector('.card__item.active');
+    cardItemActive = document.querySelector('.card__item.active'),
+    navUserName = document.getElementById('userName'),
+    navSettings = document.getElementById('nav__settings')
+
+    userName.addEventListener('click', () =>  {
+    navSettings.classList.toggle('active')
+})
