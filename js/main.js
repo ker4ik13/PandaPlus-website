6304e4c1cbf0c141d14febef5476adecaf1c1467
ker4ik13 <kekustop228@mail.ru>
2023-02-23 18:15:47 +0300

    Added settings page, added checkbox dark mode without save mode

--- js/main.js
+++ js/main.js
@@ -2,8 +2,21 @@ const cardItem = document.querySelector('.card__item'),
     cardItems = document.querySelectorAll('card__item'),
     cardItemActive = document.querySelector('.card__item.active'),
     navUserName = document.getElementById('nav__userName'),
-    navSettings = document.getElementById('nav__settings')
+    navSettings = document.getElementById('nav__settings');
 
     navUserName.addEventListener('click', () =>  {
-    navSettings.classList.toggle('active')
+    navSettings.classList.toggle('open')
 })
+
+let switchMode = document.getElementById('darkmodeButton');
+
+switchMode.onclick = function() {
+    let theme = document.getElementById('theme');
+
+    if(theme.getAttribute('href') == '../css/style.css') {
+        theme.href = '../css/darkmode-style.css';
+    } else {
+        theme.href = '../css/style.css';
+    }
+}
+
